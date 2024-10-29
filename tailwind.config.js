ea64d70d0036ec8c1628f4b5c8a873d8bc342fb4
ArnabhS <sinhaarnabh888@gmail.com>
2024-10-29 21:02:38 +0530

    landing page changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 import flowbite from "flowbite-react/tailwind";
+
 export default {
   content: [
     "./index.html",
@@ -10,10 +11,12 @@ export default {
     extend: {
       colors:{
         'primary':'rgba(255, 255, 255, 0.2)',
-        'secondary':'rgba(255, 244, 207, 0.55)'
+        'secondary':'rgba(255, 244, 207, 0.55)',
+        'custom':'rgba(36, 32, 32, 0.6)'
       },
       fontFamily: {
-       charm: ['Charm', 'cursive'], 
+       charm: ['Charm', 'cursive'],
+       italic: ['Roboto', 'italic'] 
       },
     },
   },
